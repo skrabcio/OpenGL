7caf269caa6a85ccbc0c3e312ae2a801ae9c7070
Mateusz Skrabania <skrabcio96@gmail.com>
2018-04-10 12:20:51 +0200

    Init commit

--- szablon_glut.cpp
+++ szablon_glut.cpp
@@ -12,45 +12,28 @@ using namespace std;
 
 const int WIDTH = 768;
 const int HEIGHT = 576;
-const float ROT_STEP = 10.0f;
-int model = 0; // indeks bryly 0- stozek, 1- kula, 2 - walec
+int type = 0;
 
 void onShutdown();
-void printStatus();
 void initGL();
-void changeSize(GLsizei w, GLsizei h);
-void updateProjectionMatrix();
 void renderScene();
 void keyboard(unsigned char key, int x, int y);
-void specialKeys(int key, int x, int y);
 void setupShaders();
 void setupBuffers();
 
 //******************************************************************************************
-GLuint vao; // identyfikator VAO
+GLuint vao[2]; // identyfikator VAO
 GLuint buffers[2]; // identyfikatory VBO
 
 GLuint shaderProgram; // identyfikator programu cieniowania
 
 GLuint vertexLoc; // lokalizacja atrybutu wierzcholka - wspolrzedne wierzcholka
+GLuint colorLoc;
 
-GLuint projMatrixLoc; // lokalizacja zmiennej jednorodnej - macierz projekcji
-GLuint mvMatrixLoc; // lokalizacja zmiennej jednorodnej - macierz model-widok
 
-glm::mat4 projMatrix; // macierz projekcji
-glm::mat4 mvMatrix; // macierz model-widok
-
-bool wireframe = true; // czy rysowac siatke (true) czy wypelnienie (false)
-glm::vec3 rotationAngles = glm::vec3(0.0, 0.0, 0.0); // katy rotacji wokol poszczegolnych osi
-float fovy = 45.0f; // kat patrzenia (uzywany do skalowania sceny)
-float aspectRatio = (float)WIDTH / HEIGHT;
-
-unsigned int renderElements = 0; // liczba elementow do wyrysowania
-								 //******************************************************************************************
-
-								 /*------------------------------------------------------------------------------------------
-								 ** funkcja glowna
-								 **------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------------
+** funkcja glowna
+**------------------------------------------------------------------------------------------*/
 int main(int argc, char *argv[])
 {
 	atexit(onShutdown);
@@ -132,57 +115,18 @@ void initGL()
 	printStatus();
 }
 
-/*------------------------------------------------------------------------------------------
-** funkcja wywolywana przy zmianie rozmiaru okna
-** w - aktualna szerokosc okna
-** h - aktualna wysokosc okna
-**------------------------------------------------------------------------------------------*/
-void changeSize(GLsizei w, GLsizei h)
-{
-	if (h == 0)
-		h = 1;
-	glViewport(0, 0, w, h);
-
-	aspectRatio = (float)w / h;
-
-	updateProjectionMatrix();
-}
-
-/*------------------------------------------------------------------------------------------
-** funkcja aktualizuje macierz projekcji
-**------------------------------------------------------------------------------------------*/
-void updateProjectionMatrix()
-{
-	projMatrix = glm::perspective(glm::radians(fovy), aspectRatio, 0.1f, 100.0f);
-}
 
 /*------------------------------------------------------------------------------------------
 ** funkcja rysujaca scene
 **------------------------------------------------------------------------------------------*/
 void renderScene()
 {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	mvMatrix = glm::lookAt(glm::vec3(0, 0, 8), glm::vec3(0, 0, -1), glm::vec3(0, 1, 0));
-
+	glClear(GL_COLOR_BUFFER_BIT);
 	glUseProgram(shaderProgram);
-	glUniformMatrix4fv(projMatrixLoc, 1, GL_FALSE, glm::value_ptr(projMatrix));
+	GLint lokalizacja = glGetUniformLocation(shaderProgram, "pozycja");
 
-	if (wireframe)
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-	else
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-	mvMatrix *= glm::rotate(glm::radians(rotationAngles.z), glm::vec3(0.0f, 0.0f, 1.0f));
-	mvMatrix *= glm::rotate(glm::radians(rotationAngles.y), glm::vec3(0.0f, 1.0f, 0.0f));
-	mvMatrix *= glm::rotate(glm::radians(rotationAngles.x), glm::vec3(1.0f, 0.0f, 0.0f));
-
-	glUniformMatrix4fv(mvMatrixLoc, 1, GL_FALSE, glm::value_ptr(mvMatrix));
-
-	glBindVertexArray(vao);
-	glDrawElements(GL_TRIANGLES, renderElements, GL_UNSIGNED_INT, 0);
+	
 	glBindVertexArray(0);
-
 	glutSwapBuffers();
 }
 
@@ -199,97 +143,18 @@ void keyboard(unsigned char key, int x, int y)
 		exit(0);
 		break;
 
-	case 'a':
-		rotationAngles.x -= ROT_STEP;
-		if (rotationAngles.x < 0.0f)
-			rotationAngles.x += 360.0f;
-		break;
-
-	case 'd':
-		rotationAngles.x += ROT_STEP;
-		if (rotationAngles.x > 360.0f)
-			rotationAngles.x -= 360.0f;
-		break;
-
-	case 's':
-		rotationAngles.y -= ROT_STEP;
-		if (rotationAngles.y < 0.0f)
-			rotationAngles.y += 360.0f;
-		break;
-
-	case 'w':
-		rotationAngles.y += ROT_STEP;
-		if (rotationAngles.y > 360.0f)
-			rotationAngles.y -= 360.0f;
-		break;
-
-	case 'z':
-		rotationAngles.z -= ROT_STEP;
-		if (rotationAngles.z < 0.0f)
-			rotationAngles.z += 360.f;
-		break;
-
-	case 'q':
-		rotationAngles.z += ROT_STEP;
-		if (rotationAngles.z > 360.0f)
-			rotationAngles.z -= 360.0f;
-		break;
-
-	case '+':
-	case '=':
-		fovy /= 1.1;
-		updateProjectionMatrix();
-		break;
-
-	case '-':
-		if (1.1f * fovy < 180.0f)
-		{
-			fovy *= 1.1;
-			updateProjectionMatrix();
-		}
-		break;
-
 	case '1': // stozek
-		model = 0;
-		setupBuffers();
-		renderScene();
+		type = 0;
 		break;
 
 	case '2': // kula
-		model = 1;
-		setupBuffers();
-		renderScene();
-		break;
-	case '3': // walec
-		model = 2;
-		setupBuffers();
-		renderScene();
+		type = 1;
 		break;
 	}
 	glutPostRedisplay();
+	renderScene();
 }
 
-/*------------------------------------------------------------------------------------------
-** funkcja obslugujaca klawiature (klawisze specjalne)
-** key - nacisniety klawisz
-** x, y - wspolrzedne kursora myszki w momencie nacisniecia klawisza key na klawiaturze
-**------------------------------------------------------------------------------------------*/
-void specialKeys(int key, int x, int y)
-{
-	switch (key)
-	{
-	case GLUT_KEY_F1:
-		wireframe = !wireframe;
-		break;
-
-	case GLUT_KEY_F4:
-		if (glutGetModifiers() == GLUT_ACTIVE_ALT)
-			exit(0);
-		break;
-	}
-
-	glutPostRedisplay();
-}
 
 /*------------------------------------------------------------------------------------------
 ** funkcja tworzaca program cieniowania skladajacy sie z shadera wierzcholkow i fragmentow
@@ -299,8 +164,8 @@ void setupShaders()
 	if (!setupShaders("shaders/vertex.vert", "shaders/fragment.frag", shaderProgram))
 		exit(3);
 
-	projMatrixLoc = glGetUniformLocation(shaderProgram, "projectionMatrix");
-	mvMatrixLoc = glGetUniformLocation(shaderProgram, "modelViewMatrix");
+	vertexLoc = glGetUniformLocation(shaderProgram, "vPosition");
+	colorLoc = glGetUniformLocation(shaderProgram, "vColor");
 }
 
 /*------------------------------------------------------------------------------------------
@@ -308,135 +173,6 @@ void setupShaders()
 **------------------------------------------------------------------------------------------*/
 void setupBuffers()
 {
-	vector<GLfloat> vertices;  // wierzcholki
-	vector<unsigned int> indexes; // indeksy
-	float radius = 0.5f; // promien
-	float height = 2.0f; // wysokosc
-	int prymitive = 0; // ilosc prymytiwów
-	float angle, angle2, u, v, x, y; // zmienne do obliczen
-	int vertices_base = 24, vertices_side = 24; // wierzcho³ki w podstawie i œcianie
-
-	if(model == 0) // stozek
-	{
-		int prymitive = (vertices_base + 2) * vertices_side; // obliczenie prymytiwów
-
-		for (int i = 0; i < vertices_base; i++)  
-		{
-			if ( i == 0 )
-			{
-				for (int j = 0; j < vertices_side; j++) 
-				{
-					indexes.push_back(j);
-					indexes.push_back(0);
-				}
-			}
-			for (int j = 0; j < vertices_side; j++) 
-			{  // wzor na stozek
-				x = (float)j;
-				y = (float)i;
-				u = x / (vertices_side - 1);
-				v = y / (vertices_base - 1);
-				angle = glm::radians(360 * u);
-
-				vertices.push_back(radius * (1 - v) * cos(angle));
-				vertices.push_back(radius * (1 - v) * sin(angle));
-				vertices.push_back(height * v);
-				vertices.push_back(1);
-
-				indexes.push_back(i * vertices_side + j);
-				indexes.push_back(i * vertices_side + vertices_side + j);
-			}
-			indexes.push_back(prymitive);
-		}		
-	}
-	else if (model == 1) // kula
-	{
-		for ( int i = 0; i < vertices_side; i++)
-		{
-			for (int j = 0; j < vertices_base; j++) 
-			{ // wzór na kule
-				x = (float)j;
-				y = (float)i;
-
-				if (i != (vertices_side - 1) && j != (vertices_base - 1)) 
-				{
-					indexes.push_back(i * vertices_base + vertices_base + j);
-					indexes.push_back(i * vertices_base + j + 1);
-					indexes.push_back(i * vertices_base + j);
-					indexes.push_back(i * vertices_base + j + 1);
-					indexes.push_back(i * vertices_base + vertices_base + j);
-					indexes.push_back(i * vertices_base + vertices_base + j + 1 );
-				}
-
-				u = x / (vertices_base - 1);
-				v = y / (vertices_side - 1);
-				angle = glm::radians(360 * u);
-				angle2 = glm::radians(-90 + 180 * v);
-
-				vertices.push_back(radius * cos(angle) * cos(angle2));
-				vertices.push_back(radius * sin(angle) * cos(angle2));
-				vertices.push_back(radius * sin(angle2));
-				vertices.push_back(1);
-			}
-		}
-	}
-
-	else // walec
-	{	// tworzenie prymytiwów
-		for (int i = 0; i < vertices_side; i++) {
-			if (i == 0 || i == (vertices_side - 1)) {
-				for (int j = 1; j < (vertices_base - 1); j++) {
-					indexes.push_back(i * vertices_base);
-					indexes.push_back(j + i * vertices_base);
-					indexes.push_back(j + i * vertices_base + 1);
-				}
-			}
-			for (int j = 0; j < vertices_base; j++) {
-				// wzór na walec
 	
-				x = (float)i; 
-				y = (float)j;
-				u = y / (vertices_base - 1);
-				v = x / (vertices_side - 1);
-				angle = glm::radians(360 * u);
-
-				vertices.push_back(radius * cos(angle));
-				vertices.push_back(radius * sin(angle));
-				vertices.push_back((height * v - height / 2));
-				vertices.push_back(1);
-
-				if (i != vertices_side - 1 && j != vertices_base - 1) {
-					indexes.push_back(i * vertices_base + vertices_base + j);
-					indexes.push_back(i * vertices_base + j + 1);
-					indexes.push_back(i * vertices_base + j);
-					indexes.push_back(i * vertices_base + j + 1);
-					indexes.push_back(i * vertices_base + vertices_base + j);
-					indexes.push_back(i * vertices_base + vertices_base + j + 1);
-				}
-			}
-		}
-
-	}
-	renderElements = indexes.size();
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-
-	glGenBuffers(2, buffers);
-	// VBO dla wierzcholkow
-	glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);
-	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), &vertices[0], GL_STATIC_DRAW);
-	glEnableVertexAttribArray(vertexLoc);
-	glVertexAttribPointer(vertexLoc, 4, GL_FLOAT, GL_FALSE, 0, 0);
-
-	// VBO dla indeksow
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffers[1]);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexes.size() * sizeof(GLfloat), &indexes[0], GL_STATIC_DRAW);
-	
-	if (model == 0)
-	{
-		glEnable(GL_PRIMITIVE_RESTART);
-		glPrimitiveRestartIndex(prymitive);
-	}
-	glBindVertexArray(0);
 }
 
